69958ba964429aaa5b1b732ae7f267eeb69215f0
Bogdanbg <BogdanZhila@gmail.com>
2022-12-13 11:18:52 +0200

    added ipAddress check

--- app/src/main/java/com/example/testtask/MainActivity.kt
+++ app/src/main/java/com/example/testtask/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.testtask
 
+import android.content.Context
 import android.content.Intent
+import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.view.WindowManager
+import android.text.format.Formatter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testtask.databinding.ActivityMainBinding
 import com.example.testtask.game.GameActivity
@@ -11,19 +12,39 @@ import com.example.testtask.webView.WebViewActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+
+    // private var targetIp = targetIp.add("10.0.2.16")
+    private var targetIp = mutableListOf<String>() // Here you can store a list of ip`s instead of one
+                                               // and walk through it to compare with ip of current user
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        targetIp.add("10.2.0.16")
+        targetIp.add("11.0.2.12")
+      //  targetIp.add("10.0.2.16") //commented real ip to check the condition
 
-        binding.guestButton.setOnClickListener {
-            startActivity(Intent(this, GameActivity::class.java))
-        }
+        val currentIp:String = getIp(applicationContext)
 
-        binding.moderatorBotButton.setOnClickListener {
+        if (isValid(currentIp, targetIp))
             startActivity(Intent(this, WebViewActivity::class.java))
+        else
+            startActivity(Intent(this, GameActivity::class.java))
+    }
+
+    private fun isValid(currentIp: String, addressList: MutableList<String>): Boolean{
+        var counter = false
+        for (address in addressList){
+            if (address == currentIp) counter = true
         }
+        return counter
+    }
+
+    private fun getIp(context: Context): String {
+        val wifiManager = context.applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        return Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
+
     }
 }
